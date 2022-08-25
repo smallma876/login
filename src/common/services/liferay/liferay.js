f301c64f7fc3dbd52259b78bbcd9d82af1e137fe
sergio <sergio.mallma876@gmail.com>
2022-08-24 23:19:58 -0500

    update prettier and slint

--- src/common/services/liferay/liferay.js
+++ src/common/services/liferay/liferay.js
@@ -1,8 +1,8 @@
 export const Liferay = window.Liferay || {
-	ThemeDisplay: {
-		getCompanyGroupId: () => 0,
-		getScopeGroupId: () => 0,
-		getSiteGroupId: () => 0,
-	},
-	authToken: "",
-};
+  ThemeDisplay: {
+    getCompanyGroupId: () => 0,
+    getScopeGroupId: () => 0,
+    getSiteGroupId: () => 0,
+  },
+  authToken: '',
+}
